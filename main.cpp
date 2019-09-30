9dcac1f76684ad16796a7e45d863925e61b1a46c
prestonmasseyblake <pmassey-blake@mail.csuchico.edu>
2019-09-30 16:55:30 -0700

    fileReaderFinish

--- main.cpp
+++ main.cpp
@@ -14,17 +14,21 @@ int main()
   getline(cin, fileName);
   
   //STEP 1: open the fileStream for input, using the fileName specified
-  
-  if( /*STEP 2: check to see if the fileStream successfully opened*/ )
+ fileStream.open(fileName.c_str(), ios::in);
+  if(fileStream.is_open() /*STEP 2: check to see if the fileStream successfully opened*/ )
   {
     cout<<fileName<<" opened.\nFILE CONTENTS:\n";
-    
+    do {
+        getline(fileStream,line);
+        cout<<line<<endl;
+    }
+    while (!(fileStream.eof()));
     //STEP 3: repeat the following until the end-of-file (eof) has been reached...
     // 3A: read a line from fileStream into the variable line
     // 3B: display the line, followed by an endline
   
     //STEP 4: close the fileStream
-  
+  fileStream.close();
   }
   else
   {
